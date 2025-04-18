8ec4ba9e44e1b7a37f0099081c36e61216e6f36d
松永菜月 <dicaiyue856@gmail.com>
2025-04-19 08:13:04 +0900

    変更

--- src/resources/views/auth/register.blade.php
+++ src/resources/views/auth/register.blade.php
@@ -41,7 +41,7 @@
             </label>
             <input class="register-form__input" type="password" name="password" id="password">
                 <p class="register-form__error-message">
-                    @error('email')
+                    @error('password')
                     {{ $message }}
                     @enderror
                 </p>
@@ -50,9 +50,9 @@
         <div class="register-form__group">
             <label class="register-form__label" for="password_confirmation">確認用パスワード
             </label>
-            <input class="register-form__input" type="password_confirmation" name="password_confirmation" id="password_confirmation">
+            <input class="register-form__input" type="password" name="password_confirmation" id="password_confirmation">
                 <p class="register-form__error-message">
-                    @error('password')
+                    @error('password_confirmation')
                     {{ $message }}
                     @enderror
                 </p>
