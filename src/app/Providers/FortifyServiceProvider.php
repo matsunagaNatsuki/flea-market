1ba1a853c0822f1250d896eb57fc874dfc589204
松永菜月 <dicaiyue856@gmail.com>
2025-04-21 22:26:18 +0900

    変更

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -6,12 +6,16 @@ use App\Actions\Fortify\CreateNewUser;
 use App\Actions\Fortify\ResetUserPassword;
 use App\Actions\Fortify\UpdateUserPassword;
 use App\Actions\Fortify\UpdateUserProfileInformation;
+use App\Models\User;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -44,6 +48,30 @@ class FortifyServiceProvider extends ServiceProvider
             $email =$request->input(Fortify::username());
 
             return Limit::perMinute(10)->by($email . $request->ip());
+        });
+
+        Fortify::authenticateUsing(function ($request) {
+            $validator = Validator::make($request->all(), [
+                'email' => ['required', 'email'],
+                'password' => ['required']
+            ], [
+                'email,required' => 'メールアドレスを入力してください',
+                'password.required' =>'パスワードを入力してください',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['errors' => $validator->errors()], 422);
+            }
+
+            $validated = $validator->validated();
+
+            $user = User::where('email', $validated['email'])->first();
+
+            if (!$user || !Auth::attempt(['email' => $validated['email'], 'password' => $validated['password']])) {
+                return response()->json(['errors' => ['login' => 'ログイン情報が登録されていません。']], 422);
+            }
+
+            return $user;
         });
     }
 }
