e320097aa33292776cca3852de8a6e75ddf6bee4
松永菜月 <dicaiyue856@gmail.com>
2025-04-26 21:10:33 +0900

    変更

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -3,15 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <title>Flea market</title>
     @yield('css')
 </head>
+
 <body>
-    <div class=logo.svg>
-    <img src="storage/app/public/i/logo.svg">
+    <div class="logo">
+    <img src="storage/app/public/images/logo.svg" class="logo-img">
     </div>
+
     <main>
         @yield('content')
     </main>
+
 </body>
 </html>
