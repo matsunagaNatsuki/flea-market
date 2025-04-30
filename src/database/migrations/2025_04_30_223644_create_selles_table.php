356a44f186e3faf70cca35aaba673de04aeb9cce
松永菜月 <dicaiyue856@gmail.com>
2025-04-30 23:20:35 +0900

    変更

---
+++ src/database/migrations/2025_04_30_223644_create_selles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSellesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('selles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name', 255);
+            $table->integer('price');
+            $table->string('description', 255);
+            $table->string('brand', 255);
+            $table->string('image', 255);
+            $table->unsignedBigInteger('condition_id');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('condition_id')->references('id')->on('conditions')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('selles');
+    }
+}
