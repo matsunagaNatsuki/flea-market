29ed35e188fa5364d4230cbcf1f534829e74074d
松永菜月 <dicaiyue856@gmail.com>
2025-05-03 21:18:31 +0900

    変更

--- src/app/Http/Controllers/SellController.php
+++ src/app/Http/Controllers/SellController.php
@@ -2,13 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Sell;
 use Illuminate\Http\Request;
 
 class SellController extends Controller
 {
     public function index()
     {
-        return view('index');
+        $sells = Sell::all();
+        return view('index', compact('sells'));
     }
 
     public function mylist()
--- src/app/Models/Sell.php
+++ src/app/Models/Sell.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Sell extends Model
 {
     use HasFactory;
+
+    protected $fillable =['name', 'price', 'description', 'image', 'condition_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function conditions()
+    {
+        return $this->belongsTo(Condition::class);
+    }
 }
