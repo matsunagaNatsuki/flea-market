875c5e415800800e2079c8dfeeed1d2398de8e01
松永菜月 <dicaiyue856@gmail.com>
2025-05-05 18:17:57 +0900

    変更

--- src/app/Models/Condition.php
+++ src/app/Models/Condition.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use App\Models\Sell;
 
 class Condition extends Model
 {
+
     protected $fillable = ['condition_name'];
 
     public function sells(): HasMany
