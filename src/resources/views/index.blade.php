22ed18c38e700b5af7a7ee6c66206fc86d47491d
松永菜月 <dicaiyue856@gmail.com>
2025-05-05 23:02:50 +0900

    変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,15 +1,23 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/auth/index.css') }}">
 @endsection
 
-@section('content')
-<div class="all-contents">
-        <form action="localhost/" method = "POST">
-        @csrf
-            <div class="top-contents">
+<div class="container">
+    <div class="row">
+        @foreach ($sells as $sell)
+            <div class="col-md-2 mb-4">
+                <a href="{{ url('/item/' . $sell->id) }}" >
+                    <div class="card">
+                        <img src="{{ $sell->image }}" class="card-img-top img-fluid custom-img" alt="{{ $sell->name }}">
+                        <div class="card-body">
+                            <h5 class="card-title">{{ $sell->name }}</h5>
+                        </div>
+                    </div>
+                </a>
             </div>
-
-        </form>
+        @endforeach
+    </div>
 </div>
+
