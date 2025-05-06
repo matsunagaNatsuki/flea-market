233f090f680d82292330c385e05694051ee72969
松永菜月 <dicaiyue856@gmail.com>
2025-05-06 21:21:35 +0900

    変更

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -4,20 +4,22 @@
 <link rel="stylesheet" href="{{ asset('css/auth/index.css') }}">
 @endsection
 
-<div class="container">
-    <div class="row">
-        @foreach ($sells as $sell)
-            <div class="col-md-2 mb-4">
-                <a href="{{ url('/item/' . $sell->id) }}" >
-                    <div class="card">
-                        <img src="{{ $sell->image }}" class="card-img-top img-fluid custom-img" alt="{{ $sell->name }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $sell->name }}</h5>
-                        </div>
+@section('content')
+        <div class="image-container">
+            <div class="row">
+                @foreach ($sells as $sell)
+                    <div class="col-md-2 mb-4">
+                        <a href="{{ url('/item/' . $sell->id) }}" >
+                            <div class="card">
+                                <img src="{{ $sell->image }}" class="card-img-top img-fluid custom-img" alt="{{ $sell->name }}">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $sell->name }}</h5>
+                                </div>
+                            </div>
+                        </a>
                     </div>
-                </a>
+                @endforeach
             </div>
-        @endforeach
-    </div>
-</div>
+        </div>
+@endsection
 
