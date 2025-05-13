c2bb54c585356a225088bce5addf43a39af3ea22
松永菜月 <dicaiyue856@gmail.com>
2025-05-13 12:35:27 +0900

    変更

--- src/app/Models/Category.php
+++ src/app/Models/Category.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
-{
-    use HasFactory;
+{protected $fillable = ['name'];
+
+    public function sells()
+    {
+        return $this->belongsToMany(Sell::class, 'sell_category');
+    }
 }
