f6b22875dc91b519c852acb712aa5633087402a0
松永菜月 <dicaiyue856@gmail.com>
2025-05-14 18:11:47 +0900

    変更

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -1,1 +1,12 @@
 @extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/auth/profile.css') }}">
+
+
+<div class="btn">
+<a href="mypage/profile">プロフィールを編集</a>
+</div>
+
+<div class="silver-line"></div>
+
