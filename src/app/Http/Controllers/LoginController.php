4dd9641b5dd311810c69c78bce152110ad671033
松永菜月 <dicaiyue856@gmail.com>
2025-06-02 15:48:45 +0900

    変更

---
+++ src/app/Http/Controllers/LoginController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class LoginController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request->session()->regenerate();
+
+            return redirect('/');
+        }
+    }
+
