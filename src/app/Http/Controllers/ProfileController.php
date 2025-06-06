dbc2a4baa0e297ae3891b2cb12153609404bf7ec
松永菜月 <dicaiyue856@gmail.com>
2025-06-07 00:33:20 +0900

    変更

--- src/app/Http/Controllers/ProfileController.php
+++ src/app/Http/Controllers/ProfileController.php
@@ -12,7 +12,8 @@ class ProfileController extends Controller
 {
     public function mypage()
     {
-        return view('profile');
+        $profile = Profile::where('user_id', Auth::id())->first();
+        return view('profile', compact('profile'));
     }
 
     public function showProfile()
@@ -24,15 +25,30 @@ class ProfileController extends Controller
 
     public function editProfile(Request $request)
     {
-        $profile = Profile::create([
-            'user_id' => Auth::id(),
-            'name' => $request->name,
-            'postal_code' => $request->postal_code,
-            'address' => $request->address,
-            'building' => $request->building ?? null,
-            'image' => $request->hasFile('image') ? $request->file('image')->store('profiles', 'public'): (Auth::User()->profile->image ?? null),
-        ]);
-        return redirect('/');
+        $profile = Profile::where('user_id', Auth::id())->first();
+
+        if($profile) {
+            $profile->update([
+                'name' => $request->name,
+                'postal_code' => $request->postal_code,
+                'address' => $request->address,
+                'building' => $request->building ?? null,
+                'image' => $request->hasFile('image') ? $request->file('image')->store('profiles', 'public') : ($profile->image ?? 'default-avatar.png'),
+            ]);
+        }
+
+        else {
+            Profile::create([
+                'user_id' => Auth::id(),
+                'name' => $request->name,
+                'postal_code' => $request->postal_code,
+                'address' => $request->address,
+                'building' => $request->building ?? null,
+                'image' => $request->hasFile('image') ? $request->file('image')->store('profiles', 'public') : '—Pngtree—cat default avatar_5416936.png',
+            ]);
+        }
+
+        return redirect('/mypage');
     }
 }
 
