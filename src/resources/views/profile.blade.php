8bf269bce3d57b5c8438bb5a114e6ce5679ca998
松永菜月 <dicaiyue856@gmail.com>
2025-06-07 00:33:20 +0900

    変更

--- src/resources/views/profile.blade.php
+++ src/resources/views/profile.blade.php
@@ -2,10 +2,17 @@
 
 @section('css')
 <link rel="stylesheet" href="{{ asset('css/auth/profile.css') }}">
-
+@endsection
 
 <div class="btn">
 <a href="mypage/profile">プロフィールを編集</a>
 </div>
 
 <div class="silver-line"></div>
+
+<div class="profile-container">
+    <div class="profile-image">
+        <img src="{{ asset($profile->image ? 'storage/' . $profile->image : 'images/—Pngtree—cat default avatar_5416936.png') }}" alt="{{ $profile->name }}">
+    </div>
+    <h2 class="profile-name">{{ $profile->name }}</h2>
+</div>
